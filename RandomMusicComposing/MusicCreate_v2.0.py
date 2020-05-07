431cd6895bc8045a2f3d3c0240793f20e681321c
XS_Jinbert <mr_jinbert@163.com>
2020-05-07 21:10:19 +0800

    test Verson2.0, but it is shit

---
+++ RandomMusicComposing/MusicCreate_v2.0.py
@@ -0,0 +1,195 @@
+# -*- coding = utf-8 -*-
+
+import os
+import json
+import random
+
+from midiutil.MidiFile import MIDIFile
+
+def main():
+    filename = input("请输入输出文件名：")
+    subfolder = "output"
+    if not os.path.isdir(subfolder):    # 如果文件夹不存在则建立
+        os.mkdir(subfolder)
+    name = subfolder + "/" + filename + ".mid"
+    music = Music()
+    music.create_file(filename=name,
+                      piano=True,
+                      perc=False,
+                      chord=False)
+
+class Music():
+    def __init__(self, rules_path="rules.json", length=4, tempo=90):
+        rulesPath = open(rules_path, "r")
+        rules = json.load(rulesPath)
+        rulesPath.close()
+
+        self.length = length
+        self.tempo = tempo
+
+        self.rhythms = []
+        self.rhythms.append(rules["rhythms"]["Little Star_01"])  # 读取小星星节奏规则 16拍4小节
+        self.rhythms.append(rules["rhythms"]["Little Star_02"])  # 读取小星星节奏规则 16拍4小节
+
+
+        self.seq_chord = rules["seq_chord"]  # 读取和弦规则
+        self.seq_perc = rules["seq_perc"]  # 读取击打乐规则
+        self.velocity = rules["velocity"]  # 读取速率
+
+        self.notes = rules["notes"]["01"]
+        self.interval_upper = rules["interval_upper"]
+        self.interval_lower = rules["interval_lower"]
+
+        self.MyMIDI = MIDIFile(3)   # 创建三轨道
+        self.perc_trackNumber = 0
+        self.chord_trackNumber = 1
+
+
+    # 限制音域随机音调
+    def random01_note(self):
+        last_played = 0
+        while True:
+            note = random.choice(range(1, len(self.notes) + 1))
+            if not last_played:
+                break
+            else:
+                # 音程限制
+                if random.choice(self.interval_upper) >= abs(note - last_played) >= random.choice(self.interval_lower):
+                    break
+                else:
+                    continue
+        last_played = note
+        return self.notes[last_played - 1]
+
+    # 不限制音域随机音调
+    def random02_note(self):
+        note = random.choice(range(1, len(self.notes) + 1))
+        last_played = note
+        return self.notes[last_played - 1]
+
+    # 创建打击乐
+    def create_perc_track(self):
+        self.MyMIDI.addTrackName(
+            track=self.perc_trackNumber,
+            time=0, trackName="perc")
+        self.MyMIDI.addTempo(
+            track=self.perc_trackNumber,
+            time=0, tempo=self.tempo)
+        self.MyMIDI.addProgramChange(
+            tracknum=self.perc_trackNumber,
+            channel=9, time=0, program=0)
+
+        pos = 0
+        while pos < self.length * 16:
+            if pos != 0:
+                self.MyMIDI.addNote(
+                    track=self.perc_trackNumber,
+                    channel=9, pitch=49, time=pos, duration=0.5, volume=102)
+            for pitch, duration in self.seq_perc:
+                relative_pos = pos - int(pos / 4) * 4
+                if 0 <= relative_pos < 1:
+                    vol = 102
+                elif 2 <= relative_pos < 3:
+                    vol = 96
+                else:
+                    vol = 92
+                self.MyMIDI.addNote(
+                    track=self.perc_trackNumber,
+                    channel=9, pitch=pitch, time=pos, duration=duration, volume=vol)
+                pos += duration
+
+    # 创建钢琴乐
+    def create_piano_track(self, number):
+        seq_melody = []  # 创建序列
+        for i in range(self.length):  # 读取长度
+            for phrase in self.rhythms[number]:  # 读取节奏
+                for duration in phrase:
+                    seq_melody.append((self.random01_note(), duration))  # 随机音调
+
+        self.MyMIDI.addTrackName(
+            track=number,
+            time=0, trackName="piano")
+        self.MyMIDI.addTempo(
+            track=number,
+            time=0, tempo=self.tempo)
+        self.MyMIDI.addProgramChange(
+            tracknum=number,
+            channel=0, time=0, program=0)
+
+        pos = 0
+        for pitch, duration in seq_melody:
+            relative_pos = pos - int(pos / 4) * 4
+            if 0 <= relative_pos < 1:
+                vol = self.velocity["strong"]
+            elif 2 <= relative_pos < 3:
+                vol = self.velocity["intermediate"]
+            else:
+                vol = self.velocity["weak"]
+            self.MyMIDI.addNote(
+                track=number,
+                channel=0, pitch=pitch, time=pos, duration=duration, volume=vol)
+            if relative_pos in [0, 2]:
+                self.MyMIDI.addControllerEvent(
+                    track=number,
+                    channel=0, time=pos, controller_number=64, parameter=127)
+                self.MyMIDI.addControllerEvent(
+                    track=number,
+                    channel=0, time=pos + 1.96875, controller_number=64, parameter=0)
+            pos += duration
+
+    # 创建和弦
+    def create_chord_track(self):
+        self.MyMIDI.addTrackName(
+            track=self.chord_trackNumber,
+            time=0, trackName="chords")
+        self.MyMIDI.addTempo(
+            track=self.chord_trackNumber,
+            time=0, tempo=self.tempo)
+        self.MyMIDI.addProgramChange(
+            tracknum=self.chord_trackNumber,
+            channel=0, time=0, program=0)
+
+        # C  D  E  F  G  A  B  | C  D  E  F  G  A  B  | C
+        # 48 50 52 53 55 57 59 | 60 62 64 65 67 69 71 | 72
+
+        pos = 0
+        while pos < self.length * 16:
+            for item in self.seq_chord:
+                for pitch in item:
+                    self.MyMIDI.addControllerEvent(
+                        track=self.chord_trackNumber,
+                        channel=0, time=pos, controller_number=64, parameter=127)
+                    self.MyMIDI.addControllerEvent(
+                        track=self.chord_trackNumber,
+                        channel=0, time=pos + 1.96875, controller_number=64, parameter=0)
+
+                    self.MyMIDI.addNote(
+                        track=self.chord_trackNumber,
+                        channel=0, pitch=pitch, time=pos, duration=2, volume=76)
+
+                    self.MyMIDI.addControllerEvent(
+                        track=self.chord_trackNumber,
+                        channel=0, time=pos + 2, controller_number=64, parameter=127)
+                    self.MyMIDI.addControllerEvent(
+                        track=self.chord_trackNumber,
+                        channel=0, time=pos + 3.96875, controller_number=64, parameter=0)
+
+                    self.MyMIDI.addNote(
+                        track=self.chord_trackNumber,
+                        channel=0, pitch=pitch, time=pos + 2, duration=2, volume=68)
+                pos += 4
+
+    # 创建文件（单一版本）
+    def create_file(self, filename, piano=True, chord=True, perc=True):
+        if piano:
+            self.create_piano_track(0)
+            self.create_piano_track(1)
+        if chord:
+            self.create_chord_track()
+        if perc:
+            self.create_perc_track()
+        with open(filename, "wb") as midi_file:
+            self.MyMIDI.writeFile(midi_file)
+
+if __name__ == "__main__":
+    main()
